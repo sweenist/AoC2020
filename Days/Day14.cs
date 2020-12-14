27a8a6a28d5991bb5457a4ae7c6e8c54cd703688
Ryan Sweeney <rdsweeney@sep.com>
2020-12-13 19:06:21 -0500

    Scaffolding Day 14

---
+++ Days/Day14.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Linq;
+using FluentAssertions;
+using static AdventOfCode.Utils.Extensions;
+
+namespace AdventOfCode.Days
+{
+    public static class Day14
+    {
+        private static Type _classType = typeof(Day14);
+        private static readonly string[] _sampleInput = LoadSample(_classType).ToLines();
+        private static readonly string[] _input = LoadInput(_classType).ToLines();
+        public static void Run()
+        {
+            Problem1();
+            Problem2();
+        }
+
+        private static void Problem1()
+        {
+        }
+
+        private static void Problem2()
+        {
+        }
+    }
+}
