37faece97487c0a877cf88233e14b7e43410a682
Ryan Sweeney <rdsweeney@sep.com>
2020-12-15 00:24:15 -0500

    Stubbing out Day 15... should go to bed...

---
+++ Days/Day15.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using static AdventOfCode.Utils.Extensions;
+using static AdventOfCode.Utils.BinaryExtensions;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode.Days
+{
+    public static class Day15
+    {
+        private static readonly int[] _sampleInput = new int[] { 0, 3, 6 };
+        private static readonly int[] _input = new[] { 1, 2, 16, 19, 18, 0 };
+        public static void Run()
+        {
+            Problem1();
+            Problem2();
+        }
+
+        private static void Problem1()
+        {
+        }
+
+        private static void Problem2()
+        {
+        }
+    }
+}
